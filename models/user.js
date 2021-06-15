5dc5d350f40c89f38983dbe092c58bcc876db766
Ethan Hansen <hanseneg@hotmail.com>
2021-06-15 14:17:23 -0600

    commit3

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        lowercaser: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    memberSince: {
+        type: Date,
+        default: Date.now
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+models.exports = mongoose.model('User', userSchema)
