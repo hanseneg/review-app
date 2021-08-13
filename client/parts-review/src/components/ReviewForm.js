ad98344cfb6fa770e0c1c564b6fe467fdf7929c3
Ethan Hansen <hanseneg@hotmail.com>
2021-08-13 15:47:22 -0600

    commit18

--- client/parts-review/src/components/ReviewForm.js
+++ client/parts-review/src/components/ReviewForm.js
@@ -6,71 +6,64 @@ import Star from './Star'
 //form to submit a new review
 
 export default function ReviewForm(){
-  //const { _id } = props
+  
   const { addReview } = useContext(UserContext)
   
   const initInputs = { title: "", description: "", imgUrl: "", rating: 0 }
   const [inputs, setInputs] = useState(initInputs)
 
-  
-//pass down inputs.rating as a prop to Star and set that as the value, 
-//so that every time the rating changes, the rating property of inputs will update
-
-//pass a function from ReviewForm to Star that calls setInputs, 
-//and setInputs will set the rating property to ratingValue
-
-function handleRating(rating){
-  setInputs(prevInputs => ({
-    ...prevInputs, 
-    rating: rating
-  }))
-}
-
-  function handleChange(e){
-    const {name, value} = e.target
+  function handleRating(rating){
     setInputs(prevInputs => ({
-      ...prevInputs,
-      [name]: value
+      ...prevInputs, 
+      rating: rating
     }))
   }
 
-  function handleSubmit(e){
-    e.preventDefault()
-    addReview(inputs)
-    setInputs(initInputs)
-  }
+    function handleChange(e){
+      const {name, value} = e.target
+      setInputs(prevInputs => ({
+        ...prevInputs,
+        [name]: value
+      }))
+    }
 
-  return (
-    <form className='form' onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        name="title" 
-        value={inputs.title} 
-        onChange={handleChange} 
-        placeholder="Title"
-        />
-      <textarea 
-        type="text" 
-        name="description" 
-        value={inputs.description} 
-        onChange={handleChange} 
-        placeholder="Description"
-        />
-      <input 
-        type='text'
-        name='imgUrl'
-        value={inputs.imgUrl}
-        onChange={handleChange}
-        placeholder='Image Url'
-        />
-      <Star 
-      rating={inputs.rating}
-      handleRating={handleRating}
-        />
-      <button className='button'>Add Review</button>
-      <p>The rating is {inputs.rating}.</p>
-    </form>
-  )
+    function handleSubmit(e){
+      e.preventDefault()
+      addReview(inputs)
+      setInputs(initInputs)
+    }
+
+    return (
+      <form className='form' onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name="title" 
+          value={inputs.title} 
+          onChange={handleChange} 
+          placeholder="Title"
+          />
+        <textarea 
+          type="text" 
+          name="description" 
+          value={inputs.description} 
+          onChange={handleChange} 
+          placeholder="Description"
+          />
+        <input 
+          type='text'
+          name='imgUrl'
+          value={inputs.imgUrl}
+          onChange={handleChange}
+          placeholder='Image Url'
+          />
+        <Star 
+        rating={inputs.rating}
+        handleRating={handleRating}
+          />
+        <button className='button'>Add Review</button>
+        <p>The rating is {inputs.rating}.</p>
+      </form>
+    )
 }
 
 
